42b4b4b135e89df03d2f1b7e2c373036d5561d35
Manoj <ma.manoj@gmail.com>
2017-10-10 23:42:23 +0530

    Update test.java

--- fetch ldap details/src/outlook/test.java
+++ fetch ldap details/src/outlook/test.java
@@ -5,7 +5,7 @@ public class test {
 	public static void main(String[] args) {
 		
 		//initialize
-		Outlook outlook = new Outlook("I074667", "7MYnameis");
+		Outlook outlook = new Outlook("I074667", "");
 		
 		//to get contact details
 		Contact contactDetails = outlook.getDetails("manoj.mathivanan@sap.com");
